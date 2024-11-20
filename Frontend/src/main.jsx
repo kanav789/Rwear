d47d71c8be31a5a8909d995761868cd109a157e6
kanav789 <kanavrana91@gmail.com>
2024-11-20 16:04:49 +0530

    initial the project

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,34 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./Components/Home/Home.jsx";
+import Login from "./Components/Login/Login.jsx";
+
+// Router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+    <ToastContainer />
+  </StrictMode>
+);
