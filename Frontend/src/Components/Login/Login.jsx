b983d31add6020ae424eb218036b92e8244985bf
kanav789 <kanavrana91@gmail.com>
2024-11-20 16:05:25 +0530

    Loginmodel component

---
+++ Frontend/src/Components/Login/Login.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <h3>Login</h3>
+
+      <form>
+        <input type="email" placeholder="Enter your email" required />
+        <input type="password" placeholder="Enter your password" required />
+        <input type="submit" value="Login" />
+      </form>
+      <p>
+        Don't have an account? <a href="/register">Sign Up</a>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
