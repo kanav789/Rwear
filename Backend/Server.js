1f19cc331a7558c043a244e0ab6adb61364ad043
kanav789 <kanavrana91@gmail.com>
2024-11-21 16:29:22 +0530

    adding the server file in backend

---
+++ Backend/Server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const userRouter = require("./Routes/UserRoute.js");
+const DatabaseConnected = require("./config.js");
+const app = express();
+const cors = require("cors");
+// Connect to the database
+DatabaseConnected();
+
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+app.use(cors());
+
+// Define routes
+app.use("/api/users", userRouter);
+
+const PORT = process.env.PORT || 3000;
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
