981845429351d377c7a7a6b4e83b28de62696f08
kanav789 <kanavrana91@gmail.com>
2024-11-24 17:06:35 +0530

    changes in ProductRote

--- Backend/Routes/ProductRoute.js
+++ Backend/Routes/ProductRoute.js
@@ -6,7 +6,7 @@ const TeenGuy = require("../controller/Products/TeenGuy");
 const ProductRouter = express.Router();
 
 ProductRouter.get("/mens", MensProduct);
-ProductRouter.get("/women", WomenProduct);
+ProductRouter.get("/womens", WomenProduct);
 ProductRouter.get("/Teenguy", TeenGuy);
 
 
