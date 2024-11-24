90594a6ba05f59acdac4832f2f1527ff44a7f63a
kanav789 <kanavrana91@gmail.com>
2024-11-24 17:09:33 +0530

    Adding TeenGuy code for fetching data in frontend

---
+++ Frontend/src/Pages/Teenguy/TeenGuy.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import CardRwears from "../../Components/Card/Card";
+import axios from "axios";
+function Women() {
+  const [womens, setWomens] = useState({});
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8080/api/users/Teenguy"
+        );
+        console.log(response.data.Teenguy);
+        setWomens(response.data.Teenguy);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchProduct();
+  }, []);
+
+  return (
+    <div>
+      <div className="Teenguys">
+        <CardRwears product={Teenguy} />
+      </div>
+    </div>
+  );
+}
+
+export default Women;
