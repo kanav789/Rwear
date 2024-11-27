0c2ce430252d2ebd4d4c9072b0914870366a1cad
kanav789 <kanavrana91@gmail.com>
2024-11-27 15:08:17 +0530

    Adding Overcard Route in backend

--- Backend/Routes/ProductRoute.js
+++ Backend/Routes/ProductRoute.js
@@ -2,12 +2,15 @@ const express = require("express");
 const MensProduct = require("../controller/Products/Mens");
 const WomenProduct = require("../controller/Products/Womens");
 const TeenGuy = require("../controller/Products/TeenGuy");
+const OverCard = require("../controller/Products/OverCard");
 
 const ProductRouter = express.Router();
 
 ProductRouter.get("/mens", MensProduct);
 ProductRouter.get("/womens", WomenProduct);
 ProductRouter.get("/Teenguy", TeenGuy);
+ProductRouter.get("/OverCard/:id", OverCard);
+
 
 
 module.exports = ProductRouter;
