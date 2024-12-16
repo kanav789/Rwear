6c5c4056d626aa1c98e0df3b4ac2bc9bdc594175
kanav789 <kanavrana91@gmail.com>
2024-12-16 13:30:21 +0530

    Login Functionality

--- Frontend/src/Components/Login/Login.jsx
+++ Frontend/src/Components/Login/Login.jsx
@@ -1,19 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      let token = localStorage.getItem("token");
+      if (token) {
+        return setError("User is already login");
+      }
+      const response = await axios.post(
+        "http://localhost:8080/api/users/login",
+        {
+          email,
+          password,
+        }
+      );
+      if (response.data.token) {
+        localStorage.setItem("token", response.data.token);
+        toast.success("Login Successfully");
+        navigate("/");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="login-container">
       <h3>Login</h3>
 
-      <form>
-        <input type="email" placeholder="Enter your email" required />
-        <input type="password" placeholder="Enter your password" required />
+      {/* Show error messages */}
+      {error && <h5 className="error-message">{error}</h5>}
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
         <input type="submit" value="Login" />
       </form>
-      <p>
+      <h6>
         Don't have an account? <a href="/signup">Sign Up</a>
-      </p>
+      </h6>
     </div>
   );
 };
--- Frontend/src/Components/OverCard/OverCard.jsx
+++ Frontend/src/Components/OverCard/OverCard.jsx
@@ -8,60 +8,82 @@ const OverCard = () => {
   const [product, setProduct] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:8080/api/users/overcard/${id}`
-        );
-        console.log(response.data.post);
-        setProduct(response.data.post);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching product details:", error);
-        setLoading(false);
-      }
+ useEffect(() => {
+   const fetchProduct = async () => {
+     try {
+       const response = await axios.get(
+         `http://localhost:8080/api/users/overcard/${id}`
+       );
 
-      console.log("prodct set", product);
-    };
+       setProduct(response.data.post);
+       setLoading(false);
+     } catch (error) {
+       console.error("Error fetching product details:", error);
+       setLoading(false);
+     }
+   };
 
-    fetchProduct();
-  }, [id]);
+   fetchProduct();
+ }, [id]);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+ const HandleCart = async (e) => {
+   e.preventDefault();
+   const token = localStorage.getItem("token");
+   try {
+     const sendId = await axios.post(
+       `http://localhost:8080/api/users/product/cart/${product._id}`,
+       {},
+       {
+         headers: {
+           Authorization: `Bearer ${token}`,
+         },
+       }
+     );
+     if (response) {
+       console.log(response);
+     }
+   } catch (error) {
+     console.log(error);
+   }
 
-  if (!product) {
-    return <p>Product not found.</p>;
-  }
+   console.log("hello", product._id);
+ };
 
-  return (
-    <div className="OverCard-container">
-      {/* Section */}
-      <div className="section">
-        <div className="image">
-          <img src={product.image} alt="Product" />
-        </div>
-        <div className="information">
-          <h2>{product.title}</h2>
-          <h2>Price: ₹{product.price}</h2>
-          <h2>Discount: {product.discount}</h2>
-          <h4>Size: {product.size}</h4>
+ if (loading) {
+   return <p>Loading...</p>;
+ }
 
-          <div className="button">
-            <form
-              action={`/overCard/${product._id}`}
-              method="post"
-              id="addToCartForm"
-            >
-              <input id="AddToCart" type="submit" value="Add To Cart" />
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+ if (!product) {
+   return <p>Product not found.</p>;
+ }
+
+ return (
+   <div className="OverCard-container">
+     {/* Section */}
+     <div className="section">
+       <div className="image">
+         <img src={product.image} alt="Product" />
+       </div>
+       <div className="information">
+         <h2>{product.title}</h2>
+         <h2>Price: ₹{product.price}</h2>
+         <h2>Discount: {product.discount}</h2>
+         <h4>Size: {product.size}</h4>
+
+         <div className="button">
+           <form
+             action={`/overCard/${product._id}`}
+             method="post"
+             id="addToCartForm"
+             onSubmit={HandleCart}
+           >
+             <input id="AddToCart" type="submit" value="Add To Cart" />
+           </form>
+         </div>
+       </div>
+     </div>
+   </div>
+ );
 };
 
 export default OverCard;
