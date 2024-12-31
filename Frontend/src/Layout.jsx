4ce86d84d9f73ea9163057287b3521b821a53aa9
kanav789 <kanavrana91@gmail.com>
2024-12-31 22:47:11 +0530

    Adding the Footer

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 function Layout() {
   return (
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
