a9bde779d95b31b56bdf872bccbdbb265c3717f9
kanav789 <kanavrana91@gmail.com>
2025-01-22 21:55:40 +0530

    changes

--- Frontend/src/Components/OverCard/OverCard.jsx
+++ Frontend/src/Components/OverCard/OverCard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import "./overCard.css";
+import "./OverCard.css";
 import { toast } from "react-toastify";
 import Loader from "../Loader/Loader";
 
