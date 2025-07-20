e703aaebf277345bc72919eefdc0eb5c47dac10b
kanav789 <kanavrana91@gmail.com>
2025-07-20 21:02:37 +0530

    teenGuy

--- Backend/controller/Products/TeenGuy.js
+++ Backend/controller/Products/TeenGuy.js
@@ -1,10 +1,14 @@
+const PostModel = require("../../Models/PostModel");
+
 const TeenGuy = () => async (req, res) => {
   try {
-    let teenguy = await postModel.find({ type: "teenguy" }).exec();
+    let teenguy = await PostModel.find({ type: "teenguy" }).exec();
 
-    if (teenguy) {
-      res.status(200).json({ message: "Products", teenguy });
+    if (!teenguy) {
+      return res.status(404).json({ message: "No products found" });
     }
+
+    res.status(200).json({ message: "Products", teenguy });
   } catch (error) {
     res.send(error).status(404).json({ error: "Error in Fetching Product" });
   }
---
+++ readme.md
@@ -0,0 +1,7 @@
+<!-- frontend  bugs ,  -->
+
+1 the whole page is loading at the time of navigation ✅
+2 Logout should be toggle ✅
+3 your cart should be fixed ✅
+4 tittle only be visible at the time of check details ✅
+5 font text of check details should be fixed ✅
